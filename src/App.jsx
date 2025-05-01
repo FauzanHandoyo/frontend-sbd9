2a969e8f4d96e5d01d70b698e7dac1928a7a9706
Fauzan <fauzancoding@gmail.com>
2025-05-01 22:11:02 +0700

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import MainPage from './components/mainPage';
+import MainPage from './components/MainPage';
 import ProtectedRoute from './components/ProtectedRoutes';
 
 function App() {
